7a6ad01c5a93c279080a257439bccf358679c4aa
5tka <ferol2010@yandex.ru>
2016-05-26 18:23:46 +0300

    Загальні поправки, сторінка отзивів, оплата, вакансії

--- src/js/script.js
+++ src/js/script.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
     var h=d[g]('body')[0];
     h.appendChild(s);
   }})();
-
+// ---------------слайдер для товара с миниатюрами----------------
 $('.slider-for').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
@@ -38,8 +38,14 @@ $('.slider-nav').slick({
   focusOnSelect: true
 });
 });
+// ----------------------------------------------------
 
-// ---------------слайдер для товара с миниатюрами----------------
+$('.thanksgiving__letters-gallery').slick({
+  slidesToShow: 3, 
+  slidesToScroll: 1
+});
+
+// ---------------кнопки + - для товара ----------------
 $(document).ready(function() {
       $('.product__amount .minus').click(function () {
         var $input = $(this).parent().find('input');
@@ -53,6 +59,19 @@ $(document).ready(function() {
         $input.val(parseInt($input.val()) + 1);
         return false;
       });
+
+// -------------------------------аккордион вакансии ---------------------------------------
+  $(".vacancy__accordion .vacancy__accordion-title:eq(1)").addClass("vacancy__accordion-link--active");
+  $(".vacancy__accordion .vacancy__accordion-info:not(:eq(1))").hide();
+
+  
+  $(".vacancy__accordion .vacancy__accordion-title").click(function(){
+    $(this).next(".vacancy__accordion .vacancy__accordion-info").slideToggle("slow")
+    .siblings(".vacancy__accordion .vacancy__accordion-info:visible").slideUp("slow");
+    $(this).toggleClass("vacancy__accordion-link--active");
+    $(this).siblings(".vacancy__accordion .vacancy__accordion-title")
+    .removeClass("vacancy__accordion-link--active");
+  });
     });
 // ----------------------------------ACCCORDION-----------------------
 $(".accordion__link").on('click', _accordion);
@@ -69,3 +88,60 @@ $(".accordion__link").on('click', _accordion);
       'resizeDuration': 200,
       'wrapAround': true
     })
+/////////////////////////////////////////////////////////////////////
+//=============================КАРТА==============================//
+function initialize() {
+    //получаем наш div куда будем карту добавлять
+    var mapCanvas = document.getElementById('map_canvas');
+    // задаем параметры карты
+    var mapOptions = {
+        //Это центр куда спозиционируется наша карта при загрузке
+        center: new google.maps.LatLng(59.8937928, 30.2673562),
+        //увеличение под которым будет карта, от 0 до 18
+        // 0 - минимальное увеличение - карта мира
+        // 18 - максимально детальный масштаб
+        zoom: 16, 
+        //Тип карты - обычная дорожная карта
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    //Инициализируем карту
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+ 
+    //Объявляем массив с нашими местами и маркерами
+    var markers = [],
+        myPlaces = [];
+    //Добавляем места в массив
+    myPlaces.push(new Place('Кировский район, ул. Оборонная, д.10', 59.8937928, 30.2673562, 'Санкт-Петербург')); 
+    //Теперь добавим маркеры для каждого места
+    for (var i = 0, n = myPlaces.length; i < n; i++) {
+        var marker = new google.maps.Marker({
+            //расположение на карте
+            position: new google.maps.LatLng(myPlaces[i].latitude, myPlaces[i].longitude),
+            map: map,
+            //То что мы увидим при наведении мышкой на маркер
+            title: myPlaces[i].name
+        });
+        //Добавим попап, который будет появляться при клике на маркер
+        var infowindow = new google.maps.InfoWindow({
+            content: '<h5>' + myPlaces[i].name + '</h5><br/>' + myPlaces[i].description
+        });
+        //привязываем попап к маркеру на карте
+        makeInfoWindowEvent(map, infowindow, marker);
+        markers.push(marker);
+    }
+}
+function makeInfoWindowEvent(map, infowindow, marker) {
+    //Привязываем событие КЛИК к маркеру
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map, marker);
+    });
+}
+//Это класс для удобного манипулирования местами
+function Place(name, latitude, longitude, description){
+    this.name = name;  // название
+    this.latitude = latitude;  // широта
+    this.longitude = longitude;  // долгота
+    this.description = description;  // описание места
+}
+//Когда документ загружен полностью - запускаем инициализацию карты.
+google.maps.event.addDomListener(window, 'load', initialize);
